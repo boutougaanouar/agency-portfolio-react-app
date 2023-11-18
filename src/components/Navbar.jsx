8651ce088cac4a4d00359ab8b2251361c83cb430
Anouar Boutouga <anouarboutouga2@gmail.com>
2023-11-18 14:35:07 +0100

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import profile from '../assets/images/profile.png';
+import { Link } from 'react-scroll';
+// import { Faxmark, FaBars } from "react-icons/fa6";
+
 
 const Navbar = () => {
     const [ isMenuOpen, setIsMenuOpen ] =useState(false);
@@ -27,20 +30,36 @@ const Navbar = () => {
 
         //nav array
         const navItems = [
-            {link: '#', name: 'Home'},
-            {link: '#', name: 'About'},
-            {link: '#', name: 'Services'},
-            {link: '#', name: 'Contact'},
+            {link: 'Home', path: 'Home'},
+            {link: 'About', path: 'About'},
+            {link: 'Services', path: 'Services'},
+            {link: 'Contact', path: 'Contact'},
         ];
 
   return (
     <header className='w-full bg-white md:bg-transparant fixed top-0 left-0 right-0'>
-        <nav>
-            <div>
-                <a href="" className='text-2xl fonst-semibold flex items-center space-x-3'><img src={profile} alt="boutouga" className='w-10 
-                inline-block items-center'/>
-                <span className='text-orange-700'>Boutouga</span></a>
-                <ul></ul>
+        <nav className={'py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 border-b bg-white duration-300" : "" } '}>
+            <div className='flex justify-between items-center text-base gap-8'>
+                <a href="" className='text-2xl fonst-semibold flex items-center space-x-3'>
+                    <img src={profile} alt="boutouga" className='w-10 inline-block items-center'/>
+                    <span className='text-orange-700'>Boutouga</span>
+                </a>
+                <ul className='md:flex space-x-12 hidden'>
+                    {
+                    navItems.map(({ link, path }) => (
+                        <Link to={link} spy={true} smooth={true} offset={-100} key={path} className='text-gray-500 
+                        hover:text-orange-700 first:font-medium cursor-pointer'>{link}</Link>
+                         
+                    ))}
+                </ul>
+                {/* mobil device */}
+                {/* <div className='md:hidden' >
+                    <button className='text-3xl text-neutralDGrey focus:outline-none focus:text-gray-500'>
+                        {
+                            isMenuOpen ? <Faxmark className='h-6 w-6 ' /> : <FaBars className='h-6 w-6 ' />
+                        }
+                    </button>
+                </div> */}
             </div>
         </nav>
     </header>
